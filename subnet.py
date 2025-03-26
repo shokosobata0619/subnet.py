8235a5de7346f4b8cfb1c280252fe47476ae298a
shokosobata0619 <sh8444@nyu.edu>
2025-03-25 20:57:10 -0400

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -26,22 +26,22 @@ p4First = '40.76.121.1'
 p4Last = '40.76.121.254'
 
 #problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+p5Network = '207.61.83.160'
+p5Broadcast = '207.61.83.191'
+p5First = '207.61.83.161'
+p5Last = '207.61.83.190'
 
 #problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+p6Network = '118.47.44.128'
+p6Broadcast = '118.47.44.255'
+p6First = '118.47.44.129'
+p6Last =  '118.47.44.254'
 
 #problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+p7Network = '29.32.6.0'
+p7Broadcast = '29.32.6.15'
+p7First = '29.32.6.1'
+p7Last = '29.32.6.14'
 
 #problem 8
 p8Network = ''
